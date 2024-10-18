d39bff9297fa0455d0f9f6755e08dcd4a998db38
jeison <jeisonbalduz12@gmail.com>
2024-10-18 10:57:17 -0400

    ACT configuracion

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 <?php
 
-require_once './config/app.php';
-require_once './config/sesion_start.php';
+require_once './src/config/app.php';
+require_once './src/config/sesion_start.php';
 require_once './vendor/autoload.php';
 
 error_reporting(E_ALL); // Informe de errores: informe todos los errores excepto los avisos
-
 ini_set('ignore_repeated_errors', TRUE); // Ignorar errores repetidos: siempre use TRUE
-
 ini_set('display_errors', FALSE); // Mostrar errores: use FALSE solo en entornos de producción o servidores reales. Use TRUE en entornos de desarrollo
-
 ini_set('log_errors', TRUE); // Registrar errores en archivo: activar el registro de errores en un archivo
-
 ini_set("error_log", "./php-error.log"); // Ruta del archivo de registro de errores: especificar la ruta del archivo donde se registrarán los errores
-
 error_log("Hello, errors!"); // Registrar mensaje de error: registrar un mensaje de error personalizado
 
 
---
+++ src/config/app.php
@@ -0,0 +1,9 @@
+<?php
+
+const APP_URL = "http://localhost/atlas/";
+const APP_NAME = "ATLAS";
+const APP_SESSION_NAME = "emilitorres";
+
+
+/*----------  Zona horaria  ----------*/
+date_default_timezone_set("America/Caracas");
