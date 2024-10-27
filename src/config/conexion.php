445b9d5e44286e41c9a8b3b7e92b30cf5517a9f9
Jeison Balduz <jeisonbalduz12@gmail.com>
2024-10-27 09:02:34 -0400

    actualizacion de modelo de conexion

--- src/config/conexion.php
+++ src/config/conexion.php
@@ -87,18 +87,39 @@ class Conexion extends error
     public function limpiarCadena($cadena)
     {
 
-        $palabras = ["<script>", "</script>", "<script src", "<script type=", "SELECT * FROM", "SELECT ", " SELECT ", "DELETE FROM", "INSERT INTO", "DROP TABLE", "DROP DATABASE", "TRUNCATE TABLE", "SHOW TABLES", "SHOW DATABASES", "<?php", "?>", "--", "^", "<", ">", "==", "=", ";", "::"];
-
+        $palabras = [
+            "<script>",
+            "</script>",
+            "<script src",
+            "<script type=",
+            "SELECT * FROM",
+            "SELECT",
+            "SELECT",
+            "DELETE FROM",
+            "INSERT INTO",
+            "DROP TABLE",
+            "DROP DATABASE",
+            "TRUNCATE TABLE",
+            "SHOW TABLES",
+            "SHOW DATABASES",
+            "<?php",
+            "?>",
+            "--",
+            "^",
+            "<",
+            ">",
+            "==",
+            "=",
+            ";",
+            "::"
+        ];
         $cadena = trim($cadena);
         $cadena = stripslashes($cadena);
-
         foreach ($palabras as $palabra) {
             $cadena = str_ireplace($palabra, "", $cadena);
         }
-
         $cadena = trim($cadena);
         $cadena = stripslashes($cadena);
-
         return $cadena;
     }
 
@@ -108,11 +129,39 @@ class Conexion extends error
         return !preg_match($patron, $cadena);
     }
 
+    protected function guardarDatos($tabla, $datos)
+    {
+        $query = "INSERT INTO $tabla (";
+        $C = 0;
+        foreach ($datos as $clave) {
+            if ($C >= 1) {
+                $query .= ",";
+            }
+            $query .= $clave["campo_nombre"];
+            $C++;
+        }
+        $query .= ") VALUES(";
+        $C = 0;
+        foreach ($datos as $clave) {
+            if ($C >= 1) {
+                $query .= ",";
+            }
+            $query .= $clave["campo_marcador"];
+            $C++;
+        }
+        $query .= ")";
+        $sql = $this->conectar()->prepare($query);
+        foreach ($datos as $clave) {
+            $sql->bindParam($clave["campo_marcador"], $clave["campo_valor"]);
+        }
+        $sql->execute();
+
+        return $sql;
+    }
+
     protected function actualizarDatos($tabla, $datos, $condicion)
     {
-
         $query = "UPDATE $tabla SET ";
-
         $C = 0;
         foreach ($datos as $clave) {
             if ($C >= 1) {
@@ -121,19 +170,13 @@ class Conexion extends error
             $query .= $clave["campo_nombre"] . "=" . $clave["campo_marcador"];
             $C++;
         }
-
         $query .= " WHERE " . $condicion["condicion_campo"] . "=" . $condicion["condicion_marcador"];
-
         $sql = $this->conectar()->prepare($query);
-
         foreach ($datos as $clave) {
             $sql->bindParam($clave["campo_marcador"], $clave["campo_valor"]);
         }
-
         $sql->bindParam($condicion["condicion_marcador"], $condicion["condicion_valor"]);
-
         $sql->execute();
-
         return $sql;
     }
 
@@ -143,6 +186,25 @@ class Conexion extends error
     {
         $sql = $this->conectar()->prepare("DELETE FROM $tabla WHERE $campo=:id");
         $sql->bindParam(":id", $id);
+        $sql->execute();
+
+        return $sql;
+    }
+
+    /*---------- Funcion seleccionar datos ----------*/
+    public function seleccionarDatos($tipo, $tabla, $campo, $id)
+    {
+        $tipo = $this->limpiarCadena($tipo);
+        $tabla = $this->limpiarCadena($tabla);
+        $campo = $this->limpiarCadena($campo);
+        $id = $this->limpiarCadena($id);
+
+        if ($tipo == "Unico") {
+            $sql = $this->conectar()->prepare("SELECT * FROM $tabla WHERE $campo=:ID");
+            $sql->bindParam(":ID", $id);
+        } elseif ($tipo == "Normal") {
+            $sql = $this->conectar()->prepare("SELECT $campo FROM $tabla");
+        }
         $sql->execute();
 
         return $sql;
